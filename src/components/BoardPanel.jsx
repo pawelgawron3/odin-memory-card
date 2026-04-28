225bd3ccf07579e3be32e3b0ec146627eb101d60
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-04-28 17:29:59 +0200

    Add state variables, update Card component

--- src/components/BoardPanel.jsx
+++ src/components/BoardPanel.jsx
@@ -1,12 +1,44 @@
+import { useState } from "react";
 import "../styles/boardPanel.css";
 import "../styles/card.css";
 import Card from "./Card";
 
 export default function BoardPanel({ cards }) {
+  const [firstCard, setFirstCard] = useState(null);
+  const [revealedCards, setRevealedCards] = useState([]);
+
+  function handleClick(card) {
+    if (revealedCards.includes(card.id)) return;
+
+    setRevealedCards((prev) => [...prev, card.id]);
+
+    if (!firstCard) {
+      setFirstCard(card);
+      return;
+    }
+
+    if (firstCard.pairId === card.pairId) {
+      setFirstCard(null);
+    } else {
+      setTimeout(() => {
+        setRevealedCards((prev) =>
+          prev.filter((id) => id !== card.id && id !== firstCard.id),
+        );
+      }, 500);
+
+      setFirstCard(null);
+    }
+  }
+
   return (
     <div className="boardPanel">
       {cards.map((card) => (
-        <Card key={card.id} image={card.image} />
+        <Card
+          key={card.id}
+          image={card.image}
+          revealed={revealedCards.includes(card.id)}
+          onClick={() => handleClick(card)}
+        />
       ))}
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 
-export default function Card({ image }) {
-  const [revealed, setRevealed] = useState(false);
-
+export default function Card({ image, revealed, onClick }) {
   return (
-    <div className="card" onClick={() => setRevealed(!revealed)}>
+    <div className="card" onClick={onClick}>
       {!revealed ? (
         <div className="card_back">🐾</div>
       ) : (
