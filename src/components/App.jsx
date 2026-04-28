2ab41f92032b7b7638eec85e629b168c28f3e2c4
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-04-28 12:37:56 +0200

    Add GamePanel component to structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,12 @@
+import Header from "./Header";
+import GamePanel from "./GamePanel";
 import "../styles/app.css";
 
 export default function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+      <GamePanel />
+    </div>
+  );
 }
