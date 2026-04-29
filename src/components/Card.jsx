a454681613bad298718fa827dddf3824cdf3fc32
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-04-29 12:12:23 +0200

    Refactor code

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,12 @@ import { useState } from "react";
 export default function Card({ image, revealed, onClick }) {
   return (
     <div className="card" onClick={onClick}>
-      {!revealed ? (
-        <div className="card_back">🐾</div>
-      ) : (
-        <div className="card_front">
-          <img src={image} alt="dog" />
-        </div>
-      )}
+      <div className={`card_back ${revealed ? "hidden" : ""}`}>🐾</div>
+      <img
+        src={image}
+        alt="dog"
+        className={`card_img ${revealed ? "visible" : ""}`}
+      />
     </div>
   );
 }
